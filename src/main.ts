603545dd2b5b7fb4f6c3e2538466ccda2bee065a
zhaoyudong <zhaoyudong@staff.hexun.com>
2025-10-15 13:34:12 +0800

    修改element-plus中文语言包

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import ElementPlus from 'element-plus'
 // 导入 Element Plus 样式（注意：v2.3+ 版本需单独导入样式）
 import 'element-plus/dist/index.css'
 // 导入中文语言包（可选，默认是英文）
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
